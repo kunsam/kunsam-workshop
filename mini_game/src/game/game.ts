8eace26b335ba1f5c771a20ee908d0a2c8eba200
kunsam <kusam624@icloud.com>
2021-02-05 19:07:38 +0800

    feat: change

--- mini_game/src/game/game.ts
+++ mini_game/src/game/game.ts
@@ -8,15 +8,17 @@ export default class Game {
   public currentPage: GamePage;
 
   private _ctx: CanvasRenderingContext2D;
-  private _canvas: HTMLCanvasElement;
-  public init(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+  private _canvasSize: { width: number; height: number };
+  public init(
+    ctx: CanvasRenderingContext2D,
+    canvasSize: { width: number; height: number }
+  ) {
     this._ctx = ctx;
-    this._canvas = canvas;
-
-    const gamepage1 = new GamePage1(ctx, canvas);
+    this._canvasSize = canvasSize;
+    const gamepage1 = new GamePage1(ctx);
     this.pagesMap.set(gamepage1.id, gamepage1);
 
-    const gamepage2 = new GamePage2(ctx, canvas);
+    const gamepage2 = new GamePage2(ctx);
     this.pagesMap.set(gamepage2.id, gamepage2);
 
     this.changePage(gamepage1.id);
@@ -26,16 +28,11 @@ export default class Game {
   }
 
   public clearFrame() {
-    this._ctx.clearRect(
-      0,
-      0,
-      this._canvas.clientWidth,
-      this._canvas.clientHeight
-    );
+    this._ctx.clearRect(0, 0, this._canvasSize.width, this._canvasSize.height);
   }
 
   public renderFrame: FrameRequestCallback = (time) => {
-    console.log(this.currentPageKey, "renderFrame, ");
+    console.log(this.currentPageKey, time, "renderFrame");
     this.clearFrame();
     if (this.currentPage) {
       this.currentPage.renderFrame(time);
--- mini_game/src/game/page.ts
+++ mini_game/src/game/page.ts
@@ -1,9 +1,8 @@
 export default class GamePage {
   protected _ctx: CanvasRenderingContext2D;
-  protected _canvas: HTMLCanvasElement;
-  constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+
+  constructor(ctx: CanvasRenderingContext2D) {
     this._ctx = ctx;
-    this._canvas = canvas;
   }
   public id: string;
   public renderFrame: FrameRequestCallback;
